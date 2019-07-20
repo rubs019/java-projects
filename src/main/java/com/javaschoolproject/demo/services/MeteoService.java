0ba2da403f50e1473a2b97e14bea6d77d76b0882
Tilkeii <imtilkeii@gmail.com>
2019-07-20 22:10:43 +0200

    meteo log apikey

--- src/main/java/com/javaschoolproject/demo/services/MeteoService.java
+++ src/main/java/com/javaschoolproject/demo/services/MeteoService.java
@@ -1,13 +1,18 @@
 package com.javaschoolproject.demo.services;
 
+import com.javaschoolproject.demo.Controller.MeteoController;
 import com.javaschoolproject.demo.DTO.Meteo.MeteoDto;
 import com.javaschoolproject.demo.Exceptions.ApiNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class MeteoService {
+
+    private static final Logger logger = LoggerFactory.getLogger(MeteoService.class);
 
     @Value("${api_key_optional}")
     private String API_KEY;
@@ -16,6 +21,7 @@ public class MeteoService {
 
         RestTemplate restTemplate = new RestTemplate();
 
+        logger.info("API_KEY : ".concat(API_KEY));
         if(API_KEY == null)
             throw new ApiNotFoundException();
 
